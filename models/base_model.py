5388c4aed82e56b0d2a935976f95521b7a396fd9
sungnga <708@holbertonschool.com>
2019-06-28 00:46:00 +0000

    Create initializer for BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -10,12 +10,17 @@ from datetime import datetime
 class BaseModel():
     """Class representing the BaseModel Class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         # create uuid when instance is initialized and convert to string
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-
+        if (kwargs):
+            for (key, value in kwargs.items):
+                if (key == 'created_at' or key == 'updated_at'):
+                    self.key = datetime.fromisoformat(value)
+                self.key = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at 
     def __str__(self):
         """Method that returns a string representation of an instance"""
         return ("[{}] ({}) <{}>".format(self.__class__.__name__,
