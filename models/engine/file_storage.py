cc6b93ef23f1ee85fabcce6d0ef2fe42038b8d99
sungnga <708@holbertonschool.com>
2019-06-29 00:53:49 +0000

    Fix new and save methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,25 +3,25 @@
 This module contains the FileStorage class
 """
 import json
-from ..base_model import BaseModel
+from models.base_model import BaseModel
 
 class FileStorage():
     """Class used for file storage actions"""
 
-    __file_path = ""
+    __file_path = "" + ".json"
     __objects = {}
 
     def all(self):
         return(self.__objects)
 
     def new(self, obj):
-        obj_key = "{}.{}".format(obj['__class__'], obj['id'])
-        self.__objects[obj_key] = BaseModel(**obj)
+        obj_key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[obj_key] = obj
 
     def save(self):
         with open(self.__file_path, 'w', encoding="utf-8") as fp:
             for k, v in self.__objects.items():
-                dict_ = obj.to_dict(k)
+                dict_ = self.__objects[k].to_dict()
                 json_ = json.dumps(dict_)
             fp.write(json_)
 
@@ -29,7 +29,7 @@ class FileStorage():
         try:
             with open(self.__file_path, 'r', encoding="utf-8") as fp:
                 json_str = fp.read()
-                for item in json_str:
+                for k, v in json_str.items():
                     dict_ = json.loads(item)
                     self.new(dict_)
         except Exception:
