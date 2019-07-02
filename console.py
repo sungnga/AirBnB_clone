ed54ccdebc2071cd0db5f25e1c17b5926826e886
Drew <andrew.maring@gmail.com>
2019-07-02 17:44:13 +0000

    Edit console.py for pep8

--- console.py
+++ console.py
@@ -18,23 +18,27 @@ class HBNBCommand(cmd.Cmd):
     """Class representing the HBNBcommand class"""
     prompt = '(hbnb) '
     validClasses = {'BaseModel': BaseModel,
-                'User': User,
-                'State': State,
-                'City': City,
-                'Amenity': Amenity,
-                'Place': Place,
-                'Review': Review}
+                    'User': User,
+                    'State': State,
+                    'City': City,
+                    'Amenity': Amenity,
+                    'Place': Place,
+                    'Review': Review}
 
+    objects_dict = models.storage._FileStorage__objects
 
     def do_quit(self, s):
         """A method that allows users to quit."""
         return True
+
     def help_quit(self):
         """A method that allows users to get documentation on quit."""
-        print ("Quit command to exit the program")
+        print("Quit command to exit the program")
+
     def emptyline(self):
         """A method that overwrites the default when no command is given."""
         pass
+
     do_EOF = do_quit
     help_EOF = help_quit
 
@@ -49,6 +53,7 @@ class HBNBCommand(cmd.Cmd):
             print(y.id)
         else:
             print("** class doesn't exist **")
+
     def help_create(self):
         """A method that allows users to get documentation on create."""
         print("A commend that creates an instance of a class")
@@ -61,12 +66,14 @@ class HBNBCommand(cmd.Cmd):
         elif args[0] in self.validClasses:
             if len(args) < 2:
                 print("** instance id missing **")
-            elif "{}.{}".format(args[0], args[1]) in models.storage._FileStorage__objects:
-                print(models.storage._FileStorage__objects["{}.{}".format(args[0], args[1])])
+            elif "{}.{}".format(args[0], args[1]) in HBNBCommand.objects_dict:
+                print(HBNBCommand.objects_dict["{}.{}".format(args[0],
+                                                              args[1])])
             else:
                 print("** no instance found **")
         else:
             print("** class doesn't exist **")
+
     def help_show(self):
         """A method that allows users to get documentation on show."""
         print("A command that shows the information of an instance")
@@ -79,39 +86,45 @@ class HBNBCommand(cmd.Cmd):
         elif args[0] in self.validClasses:
             if len(args) != 2:
                 print("** instance id missing **")
-            elif "{}.{}".format(args[0], args[1]) in models.storage._FileStorage__objects:
-                del(models.storage._FileStorage__objects["{}.{}".format(args[0], args[1])])
+            elif "{}.{}".format(args[0], args[1]) in HBNBCommand.objects_dict:
+                del(HBNBCommand.objects_dict["{}.{}".format(args[0], args[1])])
             else:
                 print("** no instance found **")
         else:
             print("** class doesn't exist **")
+
     def help_destroy(self):
         """A method that allows users to get documentation on destroy."""
-        print(("A command that destroys an instance based")
-        + (" on the class name and id"))
+        print(("A command that destroys an instance based") +
+              (" on the class name and id"))
 
     def do_all(self, arg):
-        """A method that prints all string representation of amodels.storage._FileStorage__objects[key]))ll instances based
-        on the class name or not."""
+        """
+        A method that prints all string representation of amodels.storage.
+        FileStorage__objects[key]))ll instances based
+        on the class name or not.
+        """
         args = shlex.split(arg)
         ret_list = []
         if not arg:
             for class_name_key in self.validClasses:
-                for key in models.storage._FileStorage__objects:
+                for key in HBNBCommand.objects_dict:
                     if class_name_key in key:
-                        ret_list.append(str(models.storage._FileStorage__objects[key]))
+                        ret_list.append(str(HBNBCommand.objects_dict[key]))
             print(ret_list)
         else:
             if args[0] in self.validClasses:
-                for key in models.storage._FileStorage__objects:
+                for key in HBNBCommand.objects_dict:
                     if arg[0] in key:
-                        ret_list.append(str(models.storage._FileStorage__objects[key]))
+                        ret_list.append(str(HBNBCommand.objects_dict[key]))
                 print(ret_list)
             else:
                 print("** class doesn't exist **")
+
     def help_all(self):
         """A method that allows users to get documentation on all."""
-        print("A command that prints all string representation of all instances")
+        print("A command that prints all string representation of all ") +
+        ("instances")
 
     def do_update(self, arg):
         """A method that updates an instance based on the class name and id
@@ -128,7 +141,7 @@ class HBNBCommand(cmd.Cmd):
         else:
             key = "{}.{}".format(args[0], args[1])
             try:
-                instance = models.storage._FileStorage__objects[key]
+                instance = HBNBCommand.objects_dict[key]
             except:
                 print("** no instance found **")
                 return
@@ -142,13 +155,18 @@ class HBNBCommand(cmd.Cmd):
                 else:
                     try:
                         type_ = type(getattr(instance, args[2]))
-                        setattr(instance, args[2], type_(args[3]))
+                        try:
+                            setattr(instance, args[2], type_(args[3]))
+                        except:
+                            print("You can't cast to type: {}".format(type_))
                     except:
                         setattr(instance, args[2], args[3])
+
     def help_update(self):
-        """ """
-        print(("A command that updates instances. Usage: update <class name>") +
-              ("<id> <attribute name> \"<attribute value>\""))
+        """A command that updates instances. Usage: update <class name> """
+        print(('A command that updates instances.\n ') +
+              ('Usage: update <class name> ') +
+              (' \<id\> \<attribute name\> \"\<attribute value\>\"'))
 
 
 if __name__ == '__main__':
