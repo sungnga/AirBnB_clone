6c761be47b6d80abd1161e4484ebfad58072ab2e
Drew <andrew.maring@gmail.com>
2019-07-03 19:28:07 +0000

    Add tests for State model

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -5,47 +5,89 @@ Module for unittests for the State class
 import unittest
 import os
 import json
+import models
+import datetime
 from models.state import State
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 
-class TestStateClassCreation(unittest.TestCase):
-    """Test class for State class instantiation tests"""
+class TestPlaceCreation(unittest.TestCase):
+    """Test class for instantiating amenity"""
 
     def setUp(self):
+        self.file = 'file.json'
+        try:
+            os.remove(self.file)
+        except:
+            pass
         self.x = State()
-        self.x.name = "CA"
-        self.x.save()
-        self.fp = open('file.json', 'r', encoding="utf-8")
-        self.dict_ = json.load(self.fp)
+        self.validAttributes = {
+            'name': str,
+            }
+        self.storage = models.storage
 
     def tearDown(self):
         try:
-            self.fp.close()
+            os.remove(self.file)
         except:
             pass
 
-    def test_state_creation(self):
-        self.assertIsInstance(self.x, BaseModel)
+    def createState(self):
+        self.ex = State()
+        self.ex.name = "North Dakota"
 
-    def test_attr_type(self):
-        self.assertIsInstance(self.x.name, str)
+    def test_has_correct_class_name(self):
+        self.assertEqual('State', self.x.__class__.__name__)
 
-    def test_attr_values(self):
-        self.assertEqual(self.x.name, "CA")
+    def test_empty_has_attrs(self):
+        for k in self.validAttributes:
+            self.assertTrue(hasattr(self.x, k))
 
-    def test_attr_is_saved(self):
-        old_updated_at = self.x.updated_at
+    def test_empty_attrs_type(self):
+        for k, v in self.validAttributes.items():
+            test_type = type(getattr(self.x, k))
+            self.assertEqual(test_type, v)
+
+    def test_added_attrs(self):
+        self.createState()
+        self.assertEqual(self.ex.name, "North Dakota")
+
+    def test_check_custom_attrs(self):
+        self.x.custom_attr = "Nga"
+        self.assertEqual(self.x.custom_attr, "Nga")
+        self.assertIsInstance(self.x.custom_attr, str)
+
+    def test_save_time_change(self):
+        old_time = self.x.updated_at
         self.x.save()
-        self.assertNotEqual(old_updated_at, self.x.updated_at)
+        self.assertNotEqual(self.x.updated_at, old_time)
 
-    def test_instance_is_in_storage(self):
-        key = "{}.{}".format(self.x.__class__.__name__, self.x.id)
-        self.assertTrue(key in self.dict_)
+    def test_new_dict(self):
+        self.createState()
+        dict_ = self.ex.to_dict()
+        self.y = State(**dict_)
+        self.assertEqual(self.ex.name, self.y.name)
 
-    def test_str_method(self):
-        string = "[{}] ({}) {}".format(self.x.__class__.__name__,
-                                       self.x.id,
-                                       self.x.__dict__)
-        self.assertEqual(string, str(self.x))
+    def test_new_dict_attr_types(self):
+        self.createState()
+        dict_ = self.ex.to_dict()
+        self.y = State(**dict_)
+        for k, v in self.validAttributes.items():
+            test_type = type(getattr(self.y, k))
+            self.assertEqual(test_type, v)
+
+    def test_save(self):
+        self.assertIsInstance(self.storage._FileStorage__objects, dict)
+        self.storage.save()
+        self.assertTrue(os.path.exists(self.file))
+        self.assertTrue(os.stat(self.file).st_size != 0)
+
+    def test_reload(self):
+        x_id = self.x.id
+        x_id_key = "{}.{}".format(self.x.__class__.__name__, self.x.id)
+        self.storage.save()
+        self.storage._FileStorage__objects = {}
+        self.storage.reload()
+        self.assertEqual(x_id,
+                         self.storage._FileStorage__objects[x_id_key].id)
