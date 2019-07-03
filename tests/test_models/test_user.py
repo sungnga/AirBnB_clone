dbf2cbdd015d29dbf9212a41b21fadd5a0bda1c7
Drew <andrew.maring@gmail.com>
2019-07-03 02:40:57 +0000

    Add tests for BaseModel attributes and method inheritance

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -64,7 +64,6 @@ class TestUserCreation(unittest.TestCase):
         self.assertTrue(hasattr(self.x, 'created_at'))
         self.assertTrue(hasattr(self.x, 'id'))
 
-
     def test_kwargs(self):
         a = User(password="psswd")
         self.assertEqual(a.password, "psswd")
@@ -94,3 +93,36 @@ class TestUserCreation(unittest.TestCase):
                                        self.x.id,
                                        self.x.__dict__)
         self.assertEqual(string, str(self.x))
+
+    def test_id_creation(self):
+        self.assertIsNotNone(self.x.id)
+        self.assertEqual(36, len(self.x.id))
+        self.assertIsInstance(self.x.id, str)
+        self.assertFalse(" " in self.x.id)
+
+    def test_created_at(self):
+        self.assertIsNotNone(self.x.created_at)
+        self.assertIsInstance(self.x.created_at, datetime.datetime)
+
+    def test_updated_at(self):
+        self.assertIsNotNone(self.x.updated_at)
+        self.assertIsInstance(self.x.updated_at, datetime.datetime)
+
+    def test_created_at_diff_updated_at(self):
+        self.assertNotEqual(self.x.updated_at, self.x.created_at)
+        time_diff = self.x.updated_at - self.x.created_at
+        self.assertTrue(time_diff.microseconds > 0)
+
+    def test_save_method(self):
+        old_time = self.x.updated_at
+        self.x.save()
+        self.assertNotEqual(old_time, self.x.updated_at)
+        self.assertIsInstance(self.x.updated_at, datetime.datetime)
+
+    def test_to_dict_method(self):
+        dict_ = self.x.to_dict()
+        self.assertIsInstance(dict_, dict)
+        self.assertIsInstance(dict_['updated_at'], str)
+        self.assertIsInstance(dict_['created_at'], str)
+        self.assertEqual(dict_['__class__'],
+                         self.x.__class__.__name__)
